1b14f2bd0a1694c0f4c959f285317947ca8962f9
Rita Nguema <marvshcherbakova@gmail.com>
2024-11-27 10:21:14 +0300

    work with module

---
+++ 10_modules.py
@@ -0,0 +1,17 @@
+import os #взаимодействие с ОС(пути)
+import time, random
+import sys
+#from
+from sys import *
+#as
+import math as m
+#собственный модуль
+import our_module
+
+print(our_module.fib)
+print(version)
+print(m.e)
+print(sys.path)
+print(time.time())
+print(random.random())
+print(os.getcwd)
