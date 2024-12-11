c6a0d55a1f4cb5e469d0eebdb31b7edf80f5645b
Rita Nguema <marvshcherbakova@gmail.com>
2024-12-11 10:51:00 +0300

    Add FILE task-5

--- 12_file_tasks.py
+++ 12_file_tasks.py
@@ -1,3 +1,4 @@
+from collections import Counter
 # #Открыть файл
 # print("Task 1 ======================================")
 # f = open('text.txt', 'r')
@@ -29,3 +30,16 @@ print("Task 4 ======================================")
 with open('text.txt') as f:
     for line in f:
         print(line)
+
+# Напишите функцию get_popular_name_from_file(filename), которая считывает файл, в котором в каждой строке записаны имя и фамилия через пробел. filename - это имя файла, в котором записаны эти имена. Вам нужно вернуть строку - самое популярное имя в файле. Если таких имен несколько, они должны быть перечислены через запятую внутри строки в алфавитном порядке.
+print("Task 5 ======================================")
+def get_popular_name_from_file(filename):
+    with open(filename, 'r') as file:
+        lines = [line.strip() for line in file.readlines()]
+        names = [line.split(' ')[0] for line in lines]
+        name_count = Counter(names)
+        max_count = max(name_count.values())
+        popular_name = [name for name, count in name_count.items() if count == max_count]
+        return ', '.join(sorted(popular_name))
+       
+
