d94efd69ff4a776409822ebb9e33f95ff0f2310e
Rita Nguema <marvshcherbakova@gmail.com>
2024-12-11 12:22:36 +0300

    Add YAML task-3

--- 14_yaml_tasks.py
+++ 14_yaml_tasks.py
@@ -18,3 +18,30 @@ with open('example.yml', 'w') as f:
 
 with open('example.yml') as f:
     print(f.read())
+
+#Реализуйте функцию create_config(bot_id, bot_token, *commands), которая создает YAML-конфигурацию для бота, используя модуль yaml. Конфигурация должна содержать следующие данные:
+
+# bot_id: Идентификатор бота (строка).
+# bot_token: Токен доступа к боту (строка).
+# commands: Одна или несколько команд, каждая из которых представлена словарем с ключами:
+# description: Описание команды (строка).
+# function: Имя функции, которая будет вызываться при выполнении команды (строка).
+# Функция должна вернуть YAML-конфигурацию бота.
+
+# Пример использования
+# bot_id = "457"
+# bot_token = "1249774028390"
+# # Пример команд
+# commands = [
+#     ("Приветствие", "greet_user"),
+#     ("Получить прогноз погоды", "get_weather")
+# ]
+print("Task 3 ======================================")
+def create_config(bot_id, bot_token, *commands):
+    config = {
+        "bot_id": bot_id,
+        "bot_token": bot_token,
+        "commands": [{"description": description, "function": function} for description, function in commands]
+    }
+    yaml_config = yaml.dump(config, allow_unicode=True)
+    return yaml_config
