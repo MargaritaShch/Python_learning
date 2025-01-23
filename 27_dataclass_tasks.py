7e7289a1132df79cc81f384c9fc7e2f3f94ff76f
Rita Nguema <marvshcherbakova@gmail.com>
2025-01-23 10:33:25 +0300

    Add DATACLASS tasks

---
+++ 27_dataclass_tasks.py
@@ -0,0 +1,54 @@
+#Dataclass
+from dataclasses import dataclass, make_dataclass
+print("Task 1 ======================================")
+@dataclass
+class SomeThing:
+    value1: int
+    value2: int
+
+s = SomeThing(1, "abc")
+print(s.value1, s.value2)
+
+
+# value3 = 5
+# @dataclass
+# class SomeThing:
+#     value1: int
+#     value2: int
+#     value3
+
+# s = SomeThing(1, "abc")
+# print(s.value1, s.value2, s.value3)
+
+#make_dataclass
+print("Task 2 ======================================")
+
+SomeThing = make_dataclass("SomeThing", ["value1", "value2"])
+s = SomeThing(1,"abc")
+print(s.value1, s.value2)
+
+#default values
+print("Task 3 ======================================")
+@dataclass
+class SomeThing:
+    value1: int =1
+    value2: int = "abc"
+
+a = SomeThing(45)
+b = SomeThing(value2="abdce")
+
+print(b.value1, a.value2)
+print(b.value1, a.value2)
+print(SomeThing.value1)
+
+#Frozen Data Class
+print("Task 4 ======================================")
+@dataclass(frozen=True)
+class SomeThing:
+    value1: int 
+    value2: str
+
+s = SomeThing(value1=256, value2="abc")
+# s.value2 = "abcde" #dataclasses.FrozenInstanceError:
+
+print(b.value1, a.value2)
